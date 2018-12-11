9ac4058345202e11caa025144930dfae77062176
haoxin <haoxinst@gmail.com>
2018-12-11 21:03:25 +0800

    update example

--- example.js
+++ example.js
@@ -1,43 +1,49 @@
 
-var koa = require('koa')
-var crypto = require('mz/crypto')
+const Koa = require('koa')
+const crypto = require('mz/crypto')
 
-var app = koa()
+const app = new Koa()
 
 require('./')(app)
 
-app.use(function* (next) {
-  var start = Date.now()
-  this.id = yield crypto.randomBytes(12)
+app.use(async function (ctx, next) {
+  const start = Date.now()
+  ctx.id = await crypto.randomBytes(12)
 
-  this.trace('start')
+  ctx.trace('start')
 
-  yield* next
+  await next()
 
-  this.trace('finish')
-  this.set('X-Response-Time', (Date.now() - start) + 'ms')
+  ctx.trace('finish')
+  ctx.set('X-Response-Time', (Date.now() - start) + 'ms')
 })
 
-app.use(function* (next) {
-  this.trace('wait:before')
-  yield function (done) {
-    setTimeout(done, Math.random() * 100)
+app.use(async function (ctx, next) {
+  ctx.trace('wait:before')
+
+  function delay () {
+    return new Promise((resolve) => {
+      setTimeout(resolve, Math.random() * 100)
+    })
   }
-  this.trace('wait:after')
 
-  yield* next
+  await delay()
+
+  ctx.trace('wait:after')
+
+  await next()
 })
 
-app.use(function* (next) {
-  this.trace('user.id', random())
-  this.trace('some.event', random(), random())
-  this.trace('another.event', random(), random())
-  this.body = 'hello world!'
+app.use(async function (ctx, next) {
+  ctx.trace('user.id', random())
+  ctx.trace('some.event', random(), random())
+  ctx.trace('another.event', random(), random())
+  ctx.body = 'hello world!'
 })
 
 app.debug()
 
-var port = process.env.PORT || 3210
+const port = process.env.PORT || 3210
 app.listen(port)
 console.log('koa-trace example listening on port %s', port)
 
